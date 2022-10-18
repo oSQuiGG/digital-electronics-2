9a8de09079533dbf845866b5a67f80ee819dc187
oSQuiGG <opmarin01@gmail.com>
2022-10-18 12:46:32 +0200

    cambios

---
+++ lab2wireless/comp2wireless.m
@@ -0,0 +1,60 @@
+close all;
+clear all;
+fc=100;
+fs=1000;
+ts=1/fs;
+t=-2:ts:2;
+s=(1+0.5*cos(2*pi*5*t)).*cos(2*pi*fc*t);
+figure(1)
+subplot(2,1,1);
+plot(t,s);
+grid on;
+title('band-pass signal');
+
+
+N=length(s);
+S=fftshift(abs(fft(s)))/N;
+df=fs/N;
+f=-fs/2:df:fs/2-df;
+subplot(2,1,2)
+plot(f,S);
+grid on;
+title('spectra of signal');
+
+
+sa=hilbert(s);
+so=sa.*exp(-j*2*pi*fc*t);
+figure(2);
+
+subplot(2,3,1);
+plot(t,real(so));
+grid on;
+title('real part of the complex envelop');
+
+subplot(2,3,2);
+plot(t,imag(so));
+title('imag part of the complex envelop');
+
+subplot(2,3,3);
+plot(t,abs(so));
+title('complex envlop');
+
+Sa=fftshift(abs(fft(sa)))/N;
+subplot(2,3,4);
+plot(f,Sa);
+grid on;
+title('spectrum of analytical signal');
+
+So=fftshift(abs(fft(so)))/N;
+subplot(2,3,5);
+plot(f,So);
+grid on;
+title('spectrum of complex envelop');
+
+snew=real(so).*cos(2*pi*fc*t)-imag(so).*sin(2*pi*fc*t);
+figure(3);
+plot(f,snew-s);
+grid on;
+title('comparison');
+m=max(snew-s);
+fprintf('\n the maximum difference between the two signals is=%i.1e \n' ,m);
---
+++ lab4-lcd/src/main.c
@@ -0,0 +1,138 @@
+/***********************************************************************
+ * 
+ * Stopwatch by Timer/Counter2 on the Liquid Crystal Display (LCD)
+
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2017 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ * Components:
+ *   16x2 character LCD with parallel interface
+ *     VSS  - GND (Power supply ground)
+ *     VDD  - +5V (Positive power supply)
+ *     Vo   - (Contrast)
+ *     RS   - PB0 (Register Select: High for Data transfer, Low for Instruction transfer)
+ *     RW   - GND (Read/Write signal: High for Read mode, Low for Write mode)
+ *     E    - PB1 (Read/Write Enable: High for Read, falling edge writes data to LCD)
+ *     D3:0 - NC (Data bits 3..0, Not Connected)
+ *     D4   - PD4 (Data bit 4)
+ *     D5   - PD5 (Data bit 5)
+ *     D6   - PD6 (Data bit 3)
+ *     D7   - PD7 (Data bit 2)
+ *     A+K  - Back-light enabled by PB2
+ * 
+ **********************************************************************/
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <lcd.h>            // Peter Fleury's LCD library
+#include <stdlib.h>         // C library. Needed for number conversions
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Update stopwatch value on LCD screen when 8-bit 
+ *           Timer/Counter2 overflows.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    // Initialize LCD display
+    lcd_init(LCD_DISP_ON_BLINK);
+
+    // Put string(s) on LCD screen
+    // lcd_gotoxy(0, 1);
+    // lcd_puts("Oscar");
+    // lcd_putc('!');
+    lcd_gotoxy(1, 0);
+    lcd_puts("00:00.0");
+    lcd_gotoxy(11, 0);
+    lcd_puts("a");
+    lcd_gotoxy(1, 1);
+    lcd_puts("b");
+    lcd_gotoxy(11, 1);
+    lcd_puts("c");
+
+    // GPIO_mode_output(&DDRB, PB2);
+    // GPIO_write_low(&PORTB, PB2);
+    
+    // Configuration of 8-bit Timer/Counter2 for Stopwatch update
+    // Set the overflow prescaler to 16 ms and enable interrupt
+
+    TIM2_overflow_16ms();
+    TIM2_overflow_interrupt_enable();
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines, ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter2 overflow interrupt
+ * Purpose:  Update the stopwatch on LCD screen every sixth overflow,
+ *           ie approximately every 100 ms (6 x 16 ms = 100 ms).
+ **********************************************************************/
+ISR(TIMER2_OVF_vect)
+{
+    static uint8_t no_of_overflows = 0;
+    static uint8_t tenths = 0;  // Tenths of a second
+    
+    char string[2];             // String for converted numbers by itoa()
+    static uint8_t secs = 0;  // seconds
+
+    no_of_overflows++;
+    if (no_of_overflows >=15)
+    {
+        // Do this every 6 x 16 ms = 100 ms
+        no_of_overflows = 0;
+
+        // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
+        tenths++;
+        if (tenths > 9)
+        {
+          tenths = 0;
+          secs++;
+          if(secs > 59)
+          {
+              secs = 0;
+          }
+          // Display seconds
+          itoa(secs, string, 10);  // Convert decimal value to string
+          lcd_gotoxy(4, 0);
+          if (secs < 10)
+          {
+                 
+              lcd_puts("0");
+          }
+          lcd_puts(string);
+        }
+        
+
+
+        itoa(tenths, string, 10);  // Convert decimal value to string
+        lcd_gotoxy(7, 0);
+        lcd_puts(string);
+        
+
+
+
+
+    }
+    // Else do nothing and exit the ISR
+}
