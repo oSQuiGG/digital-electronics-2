1e44f9dc320f6a2bab06ce2787f75f7d7fd2990b
oSQuiGG <114435572+oSQuiGG@users.noreply.github.com>
2022-12-13 23:16:58 +0100

    Update main.c

--- Final Project/src/main.c
+++ Final Project/src/main.c
@@ -7,36 +7,34 @@
 #include "timer.h"          // Timer library for AVR-GCC
 #include <gpio.h>           // GPIO library for AVR-GCC
 #include <stdlib.h>         // C library. Needed for number conversions
-#include <uart.h>
+
 
 
 /*Leds definitions*/
-#define LED1 PB3
+#define LED1 PC0
 
 
 /*Rotary encoder definitions*/
-#define outputA PB0         //Arduino pinout for CLK 
-#define outputB PB1         //Arduino pinout for DT
-#define outputSwitchEnc PB2 //Arduino pinout for encoder button
+#define outputA PB2        //Arduino pinout for CLK 
+#define outputB PB3         //Arduino pinout for DT
+#define outputSwitchEnc PB5 //Arduino pinout for encoder button
 
 
 /*Analog Joystick definitions*/
-#define outputSwitchJoy PC0  //arduino pin for the joystick button
+#define outputSwitchJoy PD2  //arduino pin for the joystick button
 #define outputAxisX PC1      //arduino pin for X axis joystick
-#define outputAxisY PD2      //arduino pin for Y axis joystick
+#define outputAxisY PC2      //arduino pin for Y axis joystick
 
 
 /*Variables (rotary encoder) definitions*/
-static uint8_t EncSwitch = 0; //Initial value if encoder button is not pushed  
-static uint8_t counter = 0; //Initial value of counter
-static uint8_t aState;
-static uint8_t aLastState;
+uint8_t EncSwitch = 0; //Initial value if encoder button is not pushed  
+uint8_t counter = 0; //Initial value of counter
+uint8_t aState;
+uint8_t aLastState;
+
+uint16_t value = 0;
 
 
-/*Variables (analog joystick) definitions*/
-static uint8_t xPosition = 0;
-static uint8_t yPosition = 0;
-static uint8_t JoySwitch = 0;
 
 
 
@@ -52,23 +50,88 @@ int main(void)
     lcd_init(LCD_DISP_ON);
 
     //Set the LED1 as output
-    GPIO_mode_output(&DDRB, LED1);
+    GPIO_mode_output(&DDRC, LED1);
     
     
     //Set the three pins of the rotary encoder as inputs
-    GPIO_mode_input_pullup(&DDRD, outputA);
-    GPIO_mode_input_pullup(&DDRD, outputB);
-    GPIO_mode_input_pullup(&DDRD, outputSwitchEnc);
-
-
-    //set the three pins of the analog joystick as inputs
-    GPIO_mode_input_pullup(&DDRB, xPosition);
-    GPIO_mode_input_pullup(&DDRB, yPosition);
-    GPIO_mode_input_pullup(&DDRB, JoySwitch);
-
+    GPIO_mode_input_pullup(&DDRB, outputA);
+    GPIO_mode_input_pullup(&DDRB, outputB);
+    GPIO_mode_input_pullup(&DDRB, outputSwitchEnc);
 
 
     
+
+
+   
+    
+   
+    
+    
+    
+
+
+    aLastState =  GPIO_read(&PINB, outputA); //read value of output A and save it in variable aLastState
+
+    char string[6]; // String for converted numbers by itoa()
+                
+    
+    while(1)
+    {
+         EncSwitch = GPIO_read(&PINB, outputSwitchEnc);
+    
+    
+    if(EncSwitch == 0 )
+    {
+        GPIO_write_low(&PORTC, LED1);  //Turn OFF the LED if EncSwitch is pressed
+        
+        lcd_gotoxy(0,0);  
+        lcd_puts("LED OFF");     //Write in LCD when 
+    }
+   
+    else if (EncSwitch == 1 )
+    {
+        GPIO_write_high(&PORTC, LED1);  //Turn ON the LED if EncSwitch is pressed
+        
+        lcd_gotoxy(0,0);
+        lcd_puts("LED  ON");
+    }
+        aState = GPIO_read(&PINB, outputA); //read value of output A and save it in variable aState
+        
+        
+        if (aState != aLastState){  //When a pulse is generated, this condition is true
+            
+            if(GPIO_read(&PINB, outputB) != aState) {  //condition to know the rotation of the encoder
+                
+                counter++;
+                
+
+            }
+
+            else {
+                
+                counter--;
+                
+            }
+            
+            
+                lcd_gotoxy(9,0);
+                lcd_puts("Pos: ");
+
+                itoa(counter, string, 10),
+                lcd_gotoxy(13,0);
+                lcd_puts("      ");
+                lcd_gotoxy(13,0);
+                lcd_puts(string);
+            
+        
+        }
+      
+        
+        aLastState = aState;
+    
+    }
+
+
     // Configure Analog-to-Digital Convertion unit
     // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
     ADMUX = ADMUX | (1<<REFS0);
@@ -84,8 +147,8 @@ int main(void)
     
     // Configure 16-bit Timer/Counter1 to start ADC conversion
     // Set prescaler to 16 ms and enable overflow interrupt
-    TIM2_overflow_16ms();
-    TIM2_overflow_interrupt_enable();
+    TIM1_overflow_16ms();
+    TIM1_overflow_interrupt_enable();
 
     
     // Enables interrupts by setting the global interrupt mask
@@ -105,100 +168,57 @@ int main(void)
 
 ISR(TIMER1_OVF_vect)
 {
-    // Start ADC conversion
-    ADCSRA = ADCSRA | (1<<ADSC);
 
-    EncSwitch = GPIO_read(&PIND, outputSwitchEnc);
+    // make conversion every 100ms
     
-    if (EncSwitch == 0 )
-    {
-        GPIO_write_low(&PORTB, LED1);  //Turn OFF the LED if EncSwitch is pressed
-        
-        lcd_gotoxy(0,1);  
-        lcd_puts("LED OFF");     //Write in LCD when 
-    }
-   
-    else if (EncSwitch == 1 )
-    {
-        GPIO_write_high(&PORTB, LED1);  //Turn ON the LED if EncSwitch is pressed
-        
-        lcd_gotoxy(0,1);
-        lcd_puts("LED ON");
-    }
-
-
-    aLastState =  GPIO_read(&PIND, outputA); //read value of output A and save it in variable aLastState
-
-    char string[6]; // String for converted numbers by itoa()
+    static uint8_t no_of_overflows = 0;
     
-    lcd_gotoxy(0,0);
-    lcd_puts("Position: ");
-
-    while(1)
-    {
-        aState = GPIO_read(&PIND, outputA); //read value of output A and save it in variable aState
-        
-        if (aState != aLastState){  //When a pulse is generated, this condition is true
-            
-            if(GPIO_read(&PIND, outputB) != aState) {  //condition to know the rotation of the encoder
-                
-                counter++;
+    no_of_overflows++;
+        if (no_of_overflows >= 6){ // Do this every 6 x 16 ms = 100 ms
+            if(value==0){
+                ADMUX = ADMUX & ~ ( 1<<MUX0); //set first channel
+                value++; //increase value to switch conversion channel
             }
-
-            else {
-                
-                counter--;
-            }
-
-            itoa(counter, string, 10),
-            lcd_gotoxy(8,0);
-            lcd_puts("       ");
-
-            lcd_gotoxy(8,0);
-            lcd_puts(string);
-        
-        }
-        
-        aLastState = aState;
-    
-    }
+            else{
+                ADMUX = ADMUX | ( 1<<MUX0); //set second conversion channel
+                value--; //decrease value to switch conversion channel
 
 }
-
+            no_of_overflows = 0;
+            ADCSRA = ADCSRA | (1<<ADSC); // Start ADC conversion
+        }
+}
 
 ISR(ADC_vect)
 {
+    uint16_t value=0; //value to switch ADC channel
+    uint16_t Xposition;
+    uint16_t Yposition;
+    
+    
+    char stringX[4];             // String for converted numbers by itoa()
+    char stringY[4];             // String for converted numbers by itoa()
+    
+    uint8_t tenthsX = 0;
+    uint8_t tenthsY = 0;
+    uint8_t secsX = 0;  // seconds AxisX
+    uint8_t secsY = 0;  // seconds AxisY
     static uint8_t no_of_overflows = 0;
-    static uint8_t tenthsX = 0;  // Tenths of a second for AxisX
-    static uint8_t tenthsY = 0;  // Tenths of a second for AxisY
-
-    
-    char stringX[2];             // String for converted numbers by itoa()
-    char stringY[2];             // String for converted numbers by itoa()
     
     
-    static uint8_t secsX = 0;  // seconds AxisX
-    static uint8_t secsY = 0;  // seconds AxisY
-
-    
-    xPosition = GPIO_read(&DDRB, outputAxisX);
-    yPosition = GPIO_read(&DDRB, outputAxisY);
-    JoySwitch = GPIO_read(&DDRB, outputSwitchJoy);
-
 
     
 
 
-    // Select input channel ADC0 (voltage divider pin)
-    /*
-    ADMUX = ADMUX & ~( 1<<MUX3 | 1<<MUX2 | 1<<MUX0 | 1<<MUX1);
-    ADCSRA |= (1<<ADSC);
-    valueX = ADC;
-    */
+   
+    
     
     no_of_overflows++;
+    if(value == 0)
+    { 
+      Xposition = ADC;
     
-    if(xPosition > 200)
+    if(Xposition > 0)
     {
         
         if (no_of_overflows >=6)
@@ -214,18 +234,22 @@ ISR(ADC_vect)
                 tenthsX = 0;
                 secsX++;
          
-                // Display seconds
+                
+        
+            }
+            // Display seconds
                 itoa(secsX, stringX, 10);  // Convert decimal value to string
                 lcd_gotoxy(9, 1);
                 lcd_puts(stringX);
-        
-            }
       
         }
     
     }
-
-    else if (yPosition > 200)
+    }
+    else
+    {
+    Yposition = ADC;
+    if (Yposition > 0)
     {
     
         if (no_of_overflows >=6)
@@ -241,18 +265,21 @@ ISR(ADC_vect)
                 tenthsY = 0;
                 secsY++;
          
-                // Display seconds
-                itoa(secsY, stringY, 10);  // Convert decimal value to string
-                lcd_gotoxy(11, 1);
-                lcd_puts(stringY);
+                
         
             }
+            // Display seconds
+                itoa(secsX, stringY, 10);  // Convert decimal value to string
+                lcd_gotoxy(9, 1);
+                lcd_puts(stringX);
+            
       
         }
   
     }
+    }
 
-    else if(xPosition > 200 && yPosition > 200)
+     /* else(xPosition > 200 && Yposition > 200)
     {
         if (no_of_overflows >=6)
         {
@@ -292,28 +319,10 @@ ISR(ADC_vect)
 
 
     }
+    */
     
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
 
 
 
